67a9faa8994dcd525f5a9a2338870e3ca9a32572
Ursu <ursula.cervantes@gmail.com>
2020-04-29 14:50:27 +0200

    removing Stripe dependencies

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -1,7 +1,4 @@
 document.addEventListener("turbolinks:load", function() {
-  const public_key = document.querySelector("meta[name='stripe-public-key']").content;
-  const stripe = Stripe(public_key);
-  const elements = stripe.elements();
 
   const style = {
   base: {
@@ -34,38 +31,6 @@ document.addEventListener("turbolinks:load", function() {
   });
 
 
-  const form = document.getElementById('new_job');
-    form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const {token, error} = await stripe.createToken(card);
-
-    if (error) {
-      // Inform the customer that there was an error.
-      const errorElement = document.getElementById('card-errors');
-      errorElement.textContent = error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(token);
-    }
-  });
-
-    const stripeTokenHandler = (token) => {
-    // Insert the token ID into the form so it gets submitted to the server
-    const form = document.getElementById('new_job');
-    const hiddenInput = document.createElement('input');
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'stripeToken');
-    hiddenInput.setAttribute('value', token.id);
-    form.appendChild(hiddenInput);
-
-    ["brand", "exp_month", "exp_year", "last4"].forEach(function(field) {
-       addFieldToForm(form, token, field);
-    });
-
-    // Submit the form
-    form.submit();
-  }
 
   function addFieldToForm(form, token, field) {
     var hiddenInput = document.createElement('input');
